7b8a88072b5676d421c17f00cfd8c9f54ffaa137
pcpnf <yufei3939@gmail.com>
2024-06-28 14:06:45 +0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$pe['db_host'] = '127.0.0.1'; //数据库主机地址
+$pe['db_name'] = '100223258'; //数据库名称
+$pe['db_user'] = '100223258'; //数据库用户名
+$pe['db_pw'] = 'xCBx7jxbXtaDzMPt'; //数据库密码
+$pe['db_coding'] = 'utf8';
+$pe['url_model'] = 'pathinfo_safe'; //url模式,可选项(pathinfo/pathinfo_safe/php)
+define('dbpre','hq_'); //数据库表前缀
+?>
